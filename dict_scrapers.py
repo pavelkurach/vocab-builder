45b961efbbda958a62ae8744673d7e215ba4f505
pavelkurach <pavel.kurach1@gmail.com>
2023-01-16 14:49:51 +0100

    added oxford learner's dictionary scraper

---
+++ dict_scrapers.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def scrape_oxford_learners_dictionary(word: str) -> list[str]:
+    url = lambda i: f'https://www.oxfordlearnersdictionaries.com/definition/english/{word}_{i}'
+    # The website filters out requests without a proper User-Agent
+    headers = {
+        'User-Agent':
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0',
+    }
+
+    list_of_definitions = []
+
+    for i in range(1, 5, 1):
+        response = requests.get(url(i), headers=headers)
+        if response.status_code != 200:
+            break
+        page = response.text
+        soup = BeautifulSoup(page, 'html.parser')
+
+        try:
+            pos = soup.find('span', class_='pos').text
+        except AttributeError:
+            pos = '-'
+
+        try:
+            senses = soup.find('ol', class_='senses_multiple')
+            list_of_senses = senses.find_all('li', class_='sense', recursive=False)
+        except AttributeError:
+            try:
+                senses = soup.find('ol', class_='sense_single')
+                list_of_senses = senses.find_all('li', class_='sense', recursive=False)
+            except AttributeError:
+                break
+
+        for sense in list_of_senses:
+            definition = sense.find('span', class_='def')
+            list_of_definitions.append(f'({pos}) ' + definition.text)
+
+    return list_of_definitions
---
+++ tests/test_dict_scrapers.py
@@ -0,0 +1,31 @@
+import unittest
+import pytest
+
+from dict_scrapers import scrape_oxford_learners_dictionary
+
+
+@pytest.mark.parametrize('word, defs', [
+    ('cat', [
+        '(noun) a small animal with soft fur that people often keep as a pet. '
+        'Cats catch and kill birds and mice.',
+        '(noun) a wild animal of the cat family',
+        '(-) a first name for girls, short for Catherine or Katherine'
+    ]),
+    ('cucumber', [
+        '(noun) a long vegetable with dark green skin that is light green inside, usually eaten raw'
+    ]),
+    ('rub', [
+        '(verb) to move your hand, a cloth, etc., '
+        'backwards and forwards over a surface while pressing it',
+        '(verb) to press two surfaces against each other and move them backwards and forwards; '
+        'to be pressed together and move in this way',
+        '(verb) to move backwards and forwards many times against something '
+        'while pressing it, especially causing pain or damage',
+        '(verb) to spread a liquid or other substance over a surface while pressing it',
+        '(noun) an act of rubbing a surface',
+        '(noun) a problem or difficulty',
+    ]),
+    ('justsomenonsense', []),
+])
+def test_scrape_oxford_learners_dictionary(word, defs):
+    assert scrape_oxford_learners_dictionary(word) == defs
